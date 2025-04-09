5d62402662ad37853f45ff00d377cdaf1cd3e677
nicoschiavon <nico.schiavon@gmail.com>
2025-04-09 17:55:00 -0300

    first commit

---
+++ src/Principal.java
@@ -0,0 +1,24 @@
+public class Principal {
+    public static void main(String[] args) {
+        Pelicula miPelicula = new Pelicula();
+        miPelicula.nombre = "Encanto";
+        miPelicula.fechaDeLanzamiento = 2021;
+        miPelicula.duracionMinutos = 120;
+        miPelicula.incluidoEnElPlan = true;
+
+        miPelicula.muestraFichaTecnica();
+        miPelicula.evalua(10);
+        miPelicula.evalua(10);
+        miPelicula.evalua(7.8);
+        System.out.println(miPelicula.getTotalDeLasEvaluaciones());
+        System.out.println(miPelicula.calculaMedia());
+
+
+        Pelicula otraPelicula = new Pelicula();
+        otraPelicula.nombre = "Matrix";
+        otraPelicula.fechaDeLanzamiento = 1998;
+        otraPelicula.duracionMinutos = 180;
+
+//        otraPelicula.muestraFichaTecnica();
+    }
+}
